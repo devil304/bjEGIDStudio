7fdba362580126d97142b6022d70a4d30cecff19
Szymon Rzosiński <Szymon Rzosiski>
2016-05-20 20:37:15 +0200

    awdrgy

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+	public worldcontroller wc;
+	// Use this for initialization
+	void Start () {
+		wc = GameObject.Find ("Main Camera").GetComponent<worldcontroller>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
