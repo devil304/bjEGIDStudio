20b49b8d1a1bcf7321df2704b865eac9589e7e54
Your Name <you@example.com>
2016-05-21 00:37:10 +0200

    jklj

--- Assets/scripts/CameraMovement.cs
+++ Assets/scripts/CameraMovement.cs
@@ -3,32 +3,44 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 
-	private float speed = 25.0f;
 	private float zoomSpeed = 2.0f;
 
-	public float minX = -560.0f;
+	public float minZ = -10.0f;
+	public float maxZ = -4.0f;
+
+	public float minX = -360.0f;
 	public float maxX = 360.0f;
 	
 	public float minY = -45.0f;
 	public float maxY = 45.0f;
 
+	public float sensX = 10.0f;
+	public float sensY = 10.0f;
+
 	void Update () {
 
 		float scroll = Input.GetAxis("Mouse ScrollWheel");
-		transform.Translate(0, scroll * zoomSpeed, scroll * zoomSpeed, Space.World);
+		transform.Translate(0, 0, scroll * zoomSpeed, Space.World);
+	
+		if (transform.position.z > maxZ) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, maxZ);
+		}else if(transform.position.z < minZ) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, minZ);
+		}
 
-		if (Input.GetKey(KeyCode.RightArrow)){
-				transform.position += Vector3.right * speed * Time.deltaTime;
-
-		}
-		if (Input.GetKey(KeyCode.LeftArrow)){
-			transform.position += Vector3.left * speed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.UpArrow)){
-			transform.position += Vector3.up * speed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.DownArrow)){
-			transform.position += Vector3.down * speed * Time.deltaTime;
+		if (Input.GetMouseButton (1)) {
+			transform.position = new Vector3 (transform.position.x - (Input.GetAxis ("Mouse X") * sensX * Time.deltaTime), transform.position.y, transform.position.z);
+			transform.position = new Vector3 (transform.position.x, transform.position.y - (Input.GetAxis ("Mouse Y") * sensY * Time.deltaTime), transform.position.z);
+			if (transform.position.x > maxX) {
+				transform.position = new Vector3 (maxX, transform.position.y, transform.position.z);
+			}else if (transform.position.x < minX) {
+				transform.position = new Vector3 (minX, transform.position.y, transform.position.z);
+			}
+			if (transform.position.y > maxY) {
+				transform.position = new Vector3 (transform.position.x, maxY, transform.position.z);
+			}else if (transform.position.y < minY) {
+				transform.position = new Vector3 (transform.position.x, minY, transform.position.z);
+			}
 		}
 	}
 }
