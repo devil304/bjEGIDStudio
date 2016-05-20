3c9fe128829219037eaa39802d7bf0f1539d7e1e
Szymon Rzosiński <Szymon Rzosiski>
2016-05-20 22:52:39 +0200

    awdfs

---
+++ Assets/scripts/hexp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hexp : MonoBehaviour {
+	public string ownername = "null";
+	public float morale;
+	public float economy;
+	public float maxlud;
+	public float lud;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -2,14 +2,33 @@
 using System.Collections;
 
 public class player : MonoBehaviour {
+	public float def;
+	public float morale;
+	public float economy;
+	public float army;
+	public int maxm;
+	public int maxa;
+	public int maxe;
+	public int minm;
+	public int mina;
+	public int mine;
+	// Use this for initialization
 	public worldcontroller wc;
 	// Use this for initialization
 	void Start () {
+		morale = Random.Range (minm, maxm);
+		economy = Random.Range (mine, maxe);
+		army = Random.Range (mina, maxa);
+
 		wc = GameObject.Find ("Main Camera").GetComponent<worldcontroller>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		def = army * (((2 * morale) + economy)/3);
+		if (wc.pt && !wc.rt) {
+			wc.rt = true;
+		}
 		if (wc.pt && wc.rt) {
 			if (Input.GetButtonDown ("Jump")) {
 				wc.pt = false;
