b21bfe4584659967e6861c7bca36771a5a6125e9
Szymon Rzosiński <Szymon Rzosiski>
2016-05-21 07:06:13 +0200

    e4ftfyuhtrgf

---
+++ Assets/scripts/buttonrec.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonrec : MonoBehaviour {
+	public string target;
+	public GameObject what;
+	public void rec(){
+		what.SendMessage (target);
+	}
+}
