6502d509ee23a93d3bac495097557881ab02d23d
Szymon Rzosiński <Szymon Rzosiski>
2016-05-21 19:47:13 +0200

    awdrgy

--- Assets/scripts/dymekdane.cs
+++ Assets/scripts/dymekdane.cs
@@ -31,7 +31,7 @@ public class dymekdane : MonoBehaviour {
 	}
 	public void kup(){
 		if (cos < pp.GetComponent<player> ().economy) {
-			pp.GetComponent<player> ().economy -= cos / 4;
+			pp.GetComponent<player> ().economy -= cos;
 			this.transform.parent.gameObject.GetComponent<hexp> ().Przejecie (pp.GetComponent<player> ().myn,pp.GetComponent<player> ().myc,pp.gameObject);
 		}
 	}
