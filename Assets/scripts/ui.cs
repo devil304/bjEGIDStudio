8b97f6c6697b83b884451ac2435f4809aab26925
Your Name <you@example.com>
2016-05-21 06:52:55 +0200

    h

--- Assets/scripts/ui.cs
+++ Assets/scripts/ui.cs
@@ -3,15 +3,15 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class ui : MonoBehaviour {
-
-
+	int licz;
+	public GameObject tur;
 	// Use this for initialization
 	void Start () {
-	
+		tur = GameObject.Find("tura");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
